51d0a399171757d0ec4c6cd28f61dd6077fa1205
Shally593 <shally.slr@gmail.com>
2023-10-07 12:47:03 +0700

    maudy

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,61 +15,57 @@ export class HomePage {
   // }
 
   ionViewDidEnter() {
-    this.map = L.map('mapId').setView(
-      [2.5162810395061546, 109.02633772363382],
-      13
-    );
+    this.map = L.map('mapId').setView([-7.781497658743231, 110.36722982606936], 10)
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    //Marker
+    const baseMaps = {
+      'OpenStreetMap' : L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }),
+
+      'Stadia' : L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+      })
+         
+    };
+    L.control.layers(baseMaps).addTo(this.map);
+
+
+    // L.marker([-7.781497658743231, 110.36722982606936]).addTo(this.map)
+    // .bindPopup('Marker1')
+    // .openPopup();
+
+    // L.marker([-7.723262973912956, 110.38033435824343]).addTo(this.map)
+    // .bindPopup('Marker1')
+    // .openPopup();
+
     const markerIcon = L.icon({
       iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png', // Ganti dengan URL ikon marker default dari CDN
       iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png', // Ganti dengan URL ikon marker default 2x dari CDN
       shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png', // Ganti dengan URL bayangan marker default dari CDN
-      iconSize: [45, 61], // Sesuaikan dengan ukuran ikon Anda
-      iconAnchor: [10, 30], // Sesuaikan dengan titik penunjuk ikon Anda
+      iconSize: [25, 41], // Sesuaikan dengan ukuran ikon Anda
+      iconAnchor: [12, 41], // Sesuaikan dengan titik penunjuk ikon Anda
     });
-  
-    const marker = L.marker([2.5162810395061546, 109.02633772363382], { icon: markerIcon }).addTo(this.map).bindPopup('Ni lagi di Natuna').openPopup();
     
-    // Base maps
-    const streetMapLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      maxZoom: 18
-    });
-
-    const satelliteMapLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    });
-
-    const terrainMapLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png', {
-      attribution: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
-    });
-
-    const watercolorMapLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
-      attribution: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
-    });
-
-    const darkMapLayer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png', {
-      attribution: 'Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
-    });
+    
+    const marker = L.marker([-7.723262973912956, 110.38033435824343], { icon: markerIcon }).addTo(this.map).bindPopup('Marker1').openPopup();
+    const marker1 = L.marker([-7.781497658743231, 110.36722982606936], { icon: markerIcon }).addTo(this.map).bindPopup('Marker2').openPopup();
+  }
 
     // Add layers to map
-    streetMapLayer.addTo(this.map);
+    // streetMapLayer.addTo(this.map);
 
-    // Layer control
-    const baseMaps = {
-      'Street Map': streetMapLayer,
-      'Satellite Map': satelliteMapLayer,
-      'Terrain Map': terrainMapLayer,
-      'Watercolor Map': watercolorMapLayer,
-      'Dark Map': darkMapLayer
-    };
+    // // Layer control
+    // const baseMaps = {
+    //   'Street Map': streetMapLayer,
+    //   'Satellite Map': satelliteMapLayer,
+    //   'Terrain Map': terrainMapLayer,
+    //   'Watercolor Map': watercolorMapLayer,
+    //   'Dark Map': darkMapLayer
+    // };
 
-    L.control.layers(baseMaps).addTo(this.map);
+    // L.control.layers(baseMaps).addTo(this.map);
   }
-}
